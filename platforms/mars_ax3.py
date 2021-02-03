f46ff5aa9515696bcee0134021a9ed81a2e66029
Nick Østergaard <oe.nick@gmail.com>
2021-02-03 18:42:22 +0100

    Initial commit to make a SoC test for the Mars AX3
    This does not build...

---
+++ platforms/mars_ax3.py
@@ -0,0 +1,126 @@
+# This file is Copyright (c) 2021 Nick Østergaard
+# License: MIT
+
+from litex.build.generic_platform import *
+from litex.build.openocd import OpenOCD
+from litex.build.xilinx import XilinxPlatform, XC3SProg, VivadoProgrammer
+
+_io = [
+    ("user_led", 0, Pins("M16"), IOStandard("LVCMOS33")),
+    ("user_led", 1, Pins("M17"), IOStandard("LVCMOS33")),
+    ("user_led", 2, Pins("L18"), IOStandard("LVCMOS33")),
+    ("user_led", 3, Pins("M18"), IOStandard("LVCMOS33")),
+
+    ("clk50", 0, Pins("P17"), IOStandard("LVCMOS33")),
+
+    # not really connected, but arty soc example uses this -- I think
+    ("cpu_reset", 0, Pins("C2"), IOStandard("LVCMOS33")),
+
+    # 2.14.5
+    # DDR3 Low Voltage Operation
+    # Low voltage operation for the DDR3 SDRAM is available only for modules revision 2 and newer.
+    # The default voltage of the DDR3 is 1.5 V. In order to enable low voltage mode (1.35 V), DDR3_VSEL (pin D9)
+    # must be driven logic 0 by the FPGA logic, and a memory voltage of 1.35 V must be selected in the Memory
+    # Interface Generator (MIG) parameters in Vivado.
+    # For 1.5 V operation, DDR3_VSEL must be set to high impedance (not driven logic 1).
+    Subsignal("ddr3_vsel", Pins("D9"), IOStandard("SSTL135")),
+
+    # TODO pmod, serial, flash, sdcard, dac?
+
+    ("serial", 0,
+        Subsignal("tx", Pins("U13")),
+        Subsignal("rx", Pins("U11")),
+        IOStandard("LVCMOS33")),
+
+    ("spiflash_4x", 0,  # clock needs to be accessed through STARTUPE2 (nick has no idea what this means)
+        Subsignal("cs_n", Pins("L13")),
+        Subsignal("dq", Pins("K17", "K18", "L14", "M14")),
+        IOStandard("LVCMOS33")
+    ),
+    ("spiflash_1x", 0,  # clock needs to be accessed through STARTUPE2 (nick has no idea what this means)
+        Subsignal("cs_n", Pins("L13")),
+        Subsignal("mosi", Pins("K17")),
+        Subsignal("miso", Pins("K18")),
+        Subsignal("wp", Pins("L14")),
+        Subsignal("hold", Pins("M14")),
+        IOStandard("LVCMOS33")
+    ),
+    # DDR3 SDRAM
+    ("ddram", 0,
+     Subsignal("a", Pins(
+         "J17 J14 J18 D18 J13 E17 K13 E18",
+         "H17 F18 G16 G18 H16 G17 H15"),
+               IOStandard("SSTL135")),
+     Subsignal("ba", Pins("D17 H14 K15"), IOStandard("SSTL135")),
+     Subsignal("ras_n", Pins("F15"), IOStandard("SSTL135")),
+     Subsignal("cas_n", Pins("F16"), IOStandard("SSTL135")),
+     Subsignal("we_n", Pins("J15"), IOStandard("SSTL135")),
+     #Subsignal("cs_n", Pins(""), IOStandard("SSTL135")), # 100R pulldown on board
+     Subsignal("dm", Pins("D15 D12"), IOStandard("SSTL135")),
+     Subsignal("dq", Pins(
+         "A18 E16 A15 E15 B18 B17 A16 B16",
+         "B14 C14 B13 D14 F13 A11 F14 B11"),
+               IOStandard("SSTL135"),
+               Misc("IN_TERM=UNTUNED_SPLIT_40")),
+     Subsignal("dqs_p", Pins("A13 C12"),
+               IOStandard("DIFF_SSTL135"),
+               Misc("IN_TERM=UNTUNED_SPLIT_40")),
+     Subsignal("dqs_n", Pins("A14 B12"),
+               IOStandard("DIFF_SSTL135"),
+               Misc("IN_TERM=UNTUNED_SPLIT_40")),
+     Subsignal("clk_p", Pins("C16"), IOStandard("DIFF_SSTL135")),
+     Subsignal("clk_n", Pins("C17"), IOStandard("DIFF_SSTL135")),
+     Subsignal("cke", Pins("G14"), IOStandard("SSTL135")),
+     Subsignal("odt", Pins("K16"), IOStandard("SSTL135")),
+     Subsignal("reset_n", Pins("G13"), IOStandard("SSTL135")),
+     Misc("SLEW=FAST"),
+     ),
+]
+
+
+class Platform(XilinxPlatform):
+    name = "mars_ax3"
+    default_clk_name = "clk50"
+    default_clk_period = 20.0
+
+    # From https://www.xilinx.com/support/documentation/user_guides/ug470_7Series_Config.pdf
+    # 17536096 bits == 2192012 == 0x21728c -- Therefore 0x220000
+    gateware_size = 0x220000
+
+    # The bigger flash device introduced starting with revision 3 has bigger erase sectors (256 kB instead of 4 kB)
+    # and the 4 kB/32 kB/64 kB erase commands are not supported anymore. Further, the programming buffer
+    # is 512 bytes instead of 256 bytes. This may require adjustments of the programming algorithm.
+    # We have Revision 3, so that is: Cypress (Spansion) S25FL512S 512 Mbit
+    # https://www.cypress.com/file/177971/download
+    # TODO, flash not tested
+    spiflash_model = "s25fl512s"
+    spiflash_read_dummy_bits = 6
+    spiflash_clock_div = 4
+    spiflash_total_size = int((512/8)*1024*1024) # 512Mbit
+    spiflash_page_size = 512
+    spiflash_sector_size = 256*1024
+
+    def __init__(self, toolchain="vivado", programmer="openocd"):
+        XilinxPlatform.__init__(self, "xc7a35t-csg324-1", _io,
+                                toolchain=toolchain)
+        self.toolchain.bitstream_commands = \
+            ["set_property BITSTREAM.CONFIG.SPI_BUSWIDTH 4 [current_design]"]
+        self.toolchain.additional_commands = \
+            ["write_cfgmem -force -format bin -interface spix4 -size 16 "
+             "-loadbit \"up 0x0 {build_name}.bit\" -file {build_name}.bin"]
+        self.programmer = programmer
+        #self.add_platform_command("set_property INTERNAL_VREF 0.750 [get_iobanks 34]")
+        self.add_platform_command("set_property INTERNAL_VREF 0.675 [get_iobanks 15]")
+
+    def create_programmer(self):
+        if self.programmer == "openocd":
+            proxy="bscan_spi_{}.bit".format(self.device.split('-')[0])
+            #return OpenOCD(config="board/digilent_arty.cfg", flash_proxy_basename=proxy)
+            return OpenOCD(config="~/nmigen_test/digilent-hs2.cfg", flash_proxy_basename=proxy)
+        elif self.programmer == "xc3sprog":
+            return XC3SProg("nexys4")
+        elif self.programmer == "vivado":
+            return VivadoProgrammer(flash_part="n25q128-3.3v-spi-x1_x2_x4")
+        else:
+            raise ValueError("{} programmer is not supported"
+                             .format(self.programmer))
---
+++ targets/mars_ax3/Makefile.mk
@@ -0,0 +1,50 @@
+# mars_ax3 targets
+
+ifneq ($(PLATFORM),mars_ax3)
+	$(error "Platform should be mars_ax3 when using this file!?")
+endif
+
+# Settings
+DEFAULT_TARGET = base
+TARGET ?= $(DEFAULT_TARGET)
+
+PROG_PORT ?= /dev/ttyUSB0
+COMM_PORT ?= /dev/ttyUSB1
+BAUD ?= 115200
+
+# Image
+image-flash-$(PLATFORM): image-flash-py
+	@true
+
+# Gateware
+gateware-load-$(PLATFORM):
+	#openocd -f board/digilent_$(PLATFORM).cfg -c "init; pld load 0 $(TARGET_BUILD_DIR)/gateware/$(PLATFORM).bit; exit"
+	#openocd -f board/digilent_arty.cfg -c "init; pld load 0 $(TARGET_BUILD_DIR)/gateware/$(PLATFORM).bit; exit"
+	openocd -f interface/ftdi/digilent-hs2.cfg -c "init; pld load 0 $(TARGET_BUILD_DIR)/gateware/$(PLATFORM).bit; exit"
+
+gateware-flash-$(PLATFORM): gateware-flash-py
+	@true
+
+# Firmware
+firmware-load-$(PLATFORM):
+	flterm --port=$(COMM_PORT) --kernel=$(FIRMWARE_FILEBASE).bin --speed=$(BAUD)
+
+
+firmware-flash-$(PLATFORM): firmwage-flash-py
+	@true
+
+firmware-connect-$(PLATFORM):
+	flterm --port=$(COMM_PORT) --speed=$(BAUD)
+
+# Bios
+bios-flash-$(PLATFORM):
+	@echo "Unsupported."
+	@false
+
+# Extra commands
+help-$(PLATFORM):
+	@true
+
+reset-$(PLATFORM):
+	@echo "Unsupported."
+	@false
